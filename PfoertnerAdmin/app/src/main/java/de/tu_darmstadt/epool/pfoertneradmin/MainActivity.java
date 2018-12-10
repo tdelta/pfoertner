67849f00331b89d3406cd4268c2b9e32e553361b
Anton Haubner <anton.haubner@outlook.de>
2018-12-10 19:12:23 +0100

    init

---
+++ PfoertnerAdmin/app/src/main/java/de/tu_darmstadt/epool/pfoertneradmin/MainActivity.java
@@ -0,0 +1,13 @@
+package de.tu_darmstadt.epool.pfoertneradmin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
