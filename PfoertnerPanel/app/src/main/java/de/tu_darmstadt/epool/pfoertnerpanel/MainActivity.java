223a8a7c70b62ef9afc5edda5c2ac1a961789fda
Anton Haubner <anton.haubner@outlook.de>
2018-12-11 12:31:46 +0100

    implemented rendering of arbitrary qr codes by employing the zxing library and writing a custom drawable for rendering

--- PfoertnerPanel/app/src/main/java/de/tu_darmstadt/epool/pfoertnerpanel/MainActivity.java
+++ PfoertnerPanel/app/src/main/java/de/tu_darmstadt/epool/pfoertnerpanel/MainActivity.java
@@ -1,7 +1,14 @@
 package de.tu_darmstadt.epool.pfoertnerpanel;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import de.tu_darmstadt.epool.pfoertnerpanel.qrcode.QRCode;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // For now, this is a simple test of QR code generation for an arbitrary text.
+        final ImageView image = findViewById(R.id.imageView);
+        final TextView text = findViewById(R.id.editText);
+        final Button button = findViewById(R.id.button);
+
+        // Whenever the set button is pressed, generate and render a QR code for the text entered by the user
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Drawable qrCode = new QRCode(text.getText().toString());
+
+                image.setImageDrawable(qrCode);
+            }
+        });
     }
 }
