b013707165c0d9d05026c935d817521085b85122
Anton Haubner <anton.haubner@outlook.de>
2018-12-21 15:30:07 +0100

    Ersten Retrofit Netwerkcode zur Initialisierung in InitializationActivity angelegt

--- PfoertnerPanel/app/src/main/java/de/tu_darmstadt/epool/pfoertnerpanel/PfoertnerService/Office.java
+++ PfoertnerPanel/app/src/main/java/de/tu_darmstadt/epool/pfoertnerpanel/PfoertnerService/Office.java
@@ -1,7 +1,7 @@
 package de.tu_darmstadt.epool.pfoertnerpanel.PfoertnerService;
 
 public class Office {
-  final int id;
+  final public int id;
 
   public Office(final int id) {
     this.id = id;
