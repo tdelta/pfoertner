7becc5b35ad94f90021033aac77ec7c2966d3e0b
Anton Haubner <anton.haubner@outlook.de>
2019-01-08 15:58:03 +0100

    Initialisierungsvorgang des Panel um Splash Screen während Ladevorgang erweitert + Integrierung der neuen Factory Methoden

--- PfoertnerPanel/app/src/main/java/de/tu_darmstadt/epool/pfoertnerpanel/InitializationActivity.java
+++ PfoertnerPanel/app/src/main/java/de/tu_darmstadt/epool/pfoertnerpanel/InitializationActivity.java
@@ -1,89 +1,32 @@
 package de.tu_darmstadt.epool.pfoertnerpanel;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.util.Consumer;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.ImageView;
 
-import java.io.IOException;
+import com.spencerwi.either.Either;
 
-import de.tu_darmstadt.epool.pfoertner.retrofit.OfficeInitConf;
+import java.util.function.Consumer;
+
 import de.tu_darmstadt.epool.pfoertner.retrofit.Password;
 import de.tu_darmstadt.epool.pfoertnerpanel.qrcode.QRCode;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
-import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import de.tu_darmstadt.epool.pfoertner.retrofit.Authentication;
-import de.tu_darmstadt.epool.pfoertner.retrofit.LoginCredentials;
 import de.tu_darmstadt.epool.pfoertner.retrofit.Office;
 import de.tu_darmstadt.epool.pfoertner.retrofit.OfficeJoinInfo;
 import de.tu_darmstadt.epool.pfoertner.retrofit.PfoertnerService;
 import de.tu_darmstadt.epool.pfoertner.retrofit.User;
 
 public class InitializationActivity extends AppCompatActivity {
-    private static User loadDevice(final PfoertnerService service, final Password password) throws IOException {
-        final User device;
-        if (false /*device already registered*/) {
-            device = null;
-        }
-
-        else {
-            // Create user
-            final Call<User> deviceCall = service.createUser(password);
-            device = deviceCall.execute().body();
-
-            if (device == null) {
-                //TODO
-            }
-        }
-
-        return device;
-    }
-
-    private static Authentication authenticate(final PfoertnerService service, final User user, final Password password) throws IOException {
-        final Authentication auth = service
-                .login(new LoginCredentials(password.password, user.id))
-                .execute()
-                .body();
-
-        if (auth == null) {
-            //TODO
-        }
-
-        return auth;
-    }
-
-    private static Office loadOffice(final PfoertnerService service, final Authentication auth) throws IOException {
-        final Office office;
-
-        if (false /*office already registered*/) {
-            office = null;
-        }
-
-        else {
-            // Create office
-            office = service
-                    .createOffice(auth.id, new OfficeInitConf("Test"))
-                    .execute()
-                    .body();
-
-            if (office == null) {
-                //TODO
-            }
-        }
-
-        return office;
-    }
-
-    private static OfficeJoinInfo loadJoinCode(final PfoertnerService service, final Office office, final Authentication auth) {
-        return new OfficeJoinInfo(office.id, "TODO"); // TODO
-    }
-
-    private void initPanel() {
+    private void initPanel(final Context context, final Consumer<Void> closeSplashScreen) {
         // Debug logging
         final HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
@@ -99,51 +42,79 @@ public class InitializationActivity extends AppCompatActivity {
         final PfoertnerService service = retrofit.create(PfoertnerService.class);
 
         new InitTask(
+                context.getSharedPreferences("registrationInfo", MODE_PRIVATE),
                 service,
-                this::showQRCode
+                e -> this.showQRCode(context, closeSplashScreen, e)
         ).execute();
     }
 
-    private static class InitTask extends AsyncTask<Void, Void, OfficeJoinInfo> {
+    private static class InitTask extends AsyncTask<Void, Void, Either<String, OfficeJoinInfo>> {
+        private final SharedPreferences registrationInfo;
         private final PfoertnerService service;
-        private final Consumer<OfficeJoinInfo> callback;
+        private final Consumer<Either<String, OfficeJoinInfo>> callback;
 
-        InitTask(final PfoertnerService service, final Consumer<OfficeJoinInfo> callback) {
+        InitTask(final SharedPreferences registrationInfo, final PfoertnerService service, final Consumer<Either<String, OfficeJoinInfo>> callback) {
+            this.registrationInfo = registrationInfo;
             this.service = service;
             this.callback = callback;
         }
 
         @Override
-        protected OfficeJoinInfo doInBackground(final Void ... parameters) {
+        protected Either<String, OfficeJoinInfo> doInBackground(final Void ... parameters) {
             try {
                 final Password password = new Password("lol");
-                final User device = loadDevice(service, password);
-                final Authentication authToken = authenticate(service, device, password);
-                final Office office = loadOffice(service, authToken);
+                final User device = User.loadDevice(registrationInfo, service, password);
+                final Authentication authToken = Authentication.authenticate(service, device, password);
+                final Office office = Office.loadOffice(registrationInfo, service, authToken);
 
-                return loadJoinCode(service, office, authToken);
+                return Either.right(
+                        OfficeJoinInfo.loadJoinCode(service, office, authToken)
+                );
             }
 
-            catch(final IOException e) {
+            catch (final RuntimeException e) {
                 e.printStackTrace();
-            }
 
-            return null;
+                return Either.left(e.getMessage());
+            }
         }
 
         @Override
-        protected void onPostExecute(final OfficeJoinInfo officeJoinInfo) {
+        protected void onPostExecute(final Either<String, OfficeJoinInfo> officeJoinInfo) {
             callback.accept(officeJoinInfo);
         }
     }
 
-    private void showQRCode(final OfficeJoinInfo data) {
-        final ImageView qrCodeView = findViewById(R.id.qrCodeView);
-        final QRCode qrCode = new QRCode(data.joinCode);
+    private void showQRCode(final Context context, final Consumer<Void> closeSplashScreen, final Either<String, OfficeJoinInfo> data) {
+        data.run(
+                errorInformation -> {
+                    final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
 
-        qrCodeView.setImageDrawable(qrCode);
+                    alertDialogBuilder.setMessage(
+                        "We encountered a problem:\n\n" +
+                                errorInformation
+                    );
+
+                    alertDialogBuilder.setPositiveButton(
+                        "Retry",
+                        (dialog, which) -> initPanel(context, closeSplashScreen)
+                    );
+
+                    alertDialogBuilder.setCancelable(false);
+
+                    alertDialogBuilder.show();
+                },
+
+                joinInfo -> {
+                    final ImageView qrCodeView = findViewById(R.id.qrCodeView);
+                    final QRCode qrCode = new QRCode(joinInfo.joinCode);
+
+                    qrCodeView.setImageDrawable(qrCode);
+
+                    closeSplashScreen.accept(null);
+                }
+        );
     }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -151,10 +122,15 @@ public class InitializationActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_initialization);
 
-        initPanel();
+        SplashScreenActivity.run(this,
+            this::initPanel
+        );
     }
 
-    public void onClick(View v) {
-        finish();
+    @Override
+    public void onBackPressed() {
+        // prevent the user from removing the initialization activity from the activity stack. (pressing back button)
+        // instead, move the entire app into the background
+        moveTaskToBack(true);
     }
 }
