5e7acffdd51ff38811cf1a07b835fb073607cbe4
Anton Haubner <anton.haubner@outlook.de>
2019-01-08 14:45:26 +0100

    Factory Methoden für Retrofit Klassen hinzugefügt. Führen Requests durch bzw. holen Daten aus dem persistenten, lokalen Speicher.

--- PfoertnerRetrofit/src/main/java/de/tu_darmstadt/epool/pfoertner/retrofit/Authentication.java
+++ PfoertnerRetrofit/src/main/java/de/tu_darmstadt/epool/pfoertner/retrofit/Authentication.java
@@ -1,20 +1,45 @@
 package de.tu_darmstadt.epool.pfoertner.retrofit;
 
-public class Authentication {
-  public final String id;
-  public final int ttl;
-  public final String created;
-  public final int userId;
+import java.io.IOException;
 
-  public Authentication(
-    final String id,
-    final int ttl,
-    final String created,
-    final int userId
-  ) {
-    this.id = id;
-    this.ttl = ttl;
-    this.created = created;
-    this.userId = userId;
-  }
+public class Authentication {
+    public final String id;
+    public final int ttl;
+    public final String created;
+    public final int userId;
+
+    public Authentication(
+            final String id,
+            final int ttl,
+            final String created,
+            final int userId
+    ) {
+        this.id = id;
+        this.ttl = ttl;
+        this.created = created;
+        this.userId = userId;
+    }
+
+    public static Authentication authenticate(final PfoertnerService service, final User user, final Password password) {
+        Authentication auth;
+
+        try {
+            auth = service
+                    .login(new LoginCredentials(password.password, user.id))
+                    .execute()
+                    .body();
+        }
+
+        catch (final IOException e) {
+            e.printStackTrace();
+            auth = null;
+            // the if below will handle further steps
+        }
+
+        if (auth == null) {
+            throw new RuntimeException("Could not authenticate at the server.");
+        }
+
+        return auth;
+    }
 }
--- PfoertnerRetrofit/src/main/java/de/tu_darmstadt/epool/pfoertner/retrofit/User.java
+++ PfoertnerRetrofit/src/main/java/de/tu_darmstadt/epool/pfoertner/retrofit/User.java
@@ -1,9 +1,48 @@
 package de.tu_darmstadt.epool.pfoertner.retrofit;
 
-public class User {
-  public final int id;
+import java.io.IOException;
+import android.content.SharedPreferences;
 
-  public User(final int id) {
-    this.id = id;
-  }
+import retrofit2.Call;
+
+public class User {
+    public final int id;
+
+    public User(final int id) {
+        this.id = id;
+    }
+
+    public static User loadDevice(final SharedPreferences deviceRegistrationInfo, final PfoertnerService service, final Password password) {
+        User device;
+
+        if (deviceRegistrationInfo.contains("UserId") /*device already registered*/) {
+            device = new User(deviceRegistrationInfo.getInt("UserId", -1));
+        }
+
+        else {
+            // Create user
+            try {
+                final Call<User> deviceCall = service.createUser(password);
+                device = deviceCall.execute().body();
+
+                if (device != null) {
+                    final SharedPreferences.Editor e = deviceRegistrationInfo.edit();
+                    e.putInt("UserId", device.id);
+                    e.apply();
+                }
+            }
+
+            catch (final IOException e) {
+                e.printStackTrace();
+                device = null;
+                // the if below will handle further steps
+            }
+        }
+
+        if (device == null) {
+            throw new RuntimeException("Could not register this device at the server.");
+        }
+
+        return device;
+    }
 }
