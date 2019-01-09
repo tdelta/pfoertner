fc36c00ff618ef9543334c2e9c2d5146c185b681
Martin <martin_x@live.de>
2019-01-09 19:54:43 +0100

    Added firebase support

--- PfoertnerServer/server/models/office.js
+++ PfoertnerServer/server/models/office.js
@@ -2,7 +2,9 @@
 
 var token = require('crypto-token');
 
-var app = require('../../server/server')
+var app = require('../../server/server');
+
+const sendFCMData = require('../firebase/firebase.js').sendFCMData;
 
 module.exports = function(Office) {
 
@@ -12,6 +14,29 @@ module.exports = function(Office) {
       instance.userJoinCode = token(32);
     }
     next();
+  });
+
+  Office.afterRemote('join', function(ctx,next) {
+    let officeId = ctx.args.id;
+    let userId = ctx.args.options.accessToken.id;
+    let fcmToken;
+    app.models.Office.findById(officeId,function(err,office){
+      if(!office) return next();
+      office.device(function(err,device){
+        if(!device) return next();
+        fcmToken = device.fcmToken;
+      });
+    });
+    app.models.Device.findById(userId,function(err,user){
+      if(!user) return next();
+      user.person(function(err,person){
+        if(!person) return next();
+        sendFCMData(fcmToken,
+          {'event': 'ADMIN_JOINED_OFFICE',
+            'admin': person});
+      });
+    });
+
   });
 
   var addUserToOffice = function(userId, officeId,cb){
@@ -60,12 +85,14 @@ module.exports = function(Office) {
     });
   }
 
-  Office.remoteMethod('join',{
+  const joinPath = {
     http: {path: '/:id/join', verb: 'put'},
     accepts: [
       {arg: 'joinCode', type: 'string', required: 'true'},
       {arg: 'id', type: 'number', required: 'true'},
       {"arg": "options", "type": "object", "http": "optionsFromRequest"}
     ]
-  });
+  };
+
+  Office.remoteMethod('join',joinPath);
 }
