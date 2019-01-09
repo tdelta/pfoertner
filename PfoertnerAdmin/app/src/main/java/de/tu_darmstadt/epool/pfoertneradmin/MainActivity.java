0030ba6315af2b4515dbfa283e3fa563925e4965
Jonas Belouadi <bigjonas96@gmail.com>
2019-01-09 19:47:08 +0100

    Globaler Status lässt sich ändern (zumindest Adminseitig)

--- PfoertnerAdmin/app/src/main/java/de/tu_darmstadt/epool/pfoertneradmin/MainActivity.java
+++ PfoertnerAdmin/app/src/main/java/de/tu_darmstadt/epool/pfoertneradmin/MainActivity.java
@@ -1,10 +1,14 @@
 package de.tu_darmstadt.epool.pfoertneradmin;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import de.tu_darmstadt.epool.pfoertner.common.retrofit.Authentication;
 import de.tu_darmstadt.epool.pfoertner.common.retrofit.PfoertnerService;
@@ -16,9 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
     private SharedPreferences settings;
     private PfoertnerService service;
-    private State state = State.getInstance();
+    private DialogFragment globalStatusMenu;
 
-    private Authentication authtoken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +31,10 @@ public class MainActivity extends AppCompatActivity {
         //create retrofit client
         service =  State.getInstance().service;
 
-
         settings = getSharedPreferences("Settings", 0);
+
+        globalStatusMenu = StatusFragment.newInstance(this);
+
 
         // "Proof of concept" for persistence variable in memory
         if (settings.getString("token",null ) == null){
@@ -44,4 +49,12 @@ public class MainActivity extends AppCompatActivity {
             something.show();
         }
     }
+
+    public void editGlobalInfo(View view){
+        globalStatusMenu.show(getSupportFragmentManager(), "globalStatusMenu");
+
+    }
+
+
+
 }
---
+++ PfoertnerAdmin/app/src/main/java/de/tu_darmstadt/epool/pfoertneradmin/StatusFragment.java
@@ -0,0 +1,79 @@
+package de.tu_darmstadt.epool.pfoertneradmin;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.util.ArraySet;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+public class StatusFragment extends DialogFragment{
+    public int selected;
+    private int tempSelected;
+    private List<String> status;
+    private SharedPreferences settings;
+    TextView textfield;
+
+
+        public static StatusFragment newInstance(Activity activity){
+            StatusFragment fragment = new StatusFragment();
+            fragment.setArguments(activity);
+            return fragment;
+        }
+
+        public void setArguments(Activity activity){
+            settings = activity.getPreferences( 0);
+            textfield = activity.findViewById(R.id.summary);
+            selected = settings.getInt("globalStatusSelected", 0);
+
+            if (settings.contains("globalStatusModes")){
+                Gson gson = new Gson();
+                String statusJSON =  settings.getString("globalStatusModes", null);
+                status = Arrays.asList(gson.fromJson(statusJSON, String[].class));
+            }
+            else{
+                status = new ArrayList<String>();
+                status.add("Do Not Disturb!");
+                status.add("Come In!");
+                status.add("Only Urgent Matters!");
+            }
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle("Select Status")
+                    .setSingleChoiceItems(status.toArray(new String[0]), selected,new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            tempSelected = which;
+                        }
+                    })
+                    .setPositiveButton( "OK", new DialogInterface.OnClickListener() {
+                        public void onClick( DialogInterface dialog, int whichButton)
+                        {
+                            selected = tempSelected;
+                            final SharedPreferences.Editor e = settings.edit();
+                            e.putInt("globalStatusSelected", selected);
+                            textfield.setText("Current: " + status.get(selected));
+                            e.apply();
+
+
+                        }
+                    });
+            return builder.create();
+        }
+
+}
