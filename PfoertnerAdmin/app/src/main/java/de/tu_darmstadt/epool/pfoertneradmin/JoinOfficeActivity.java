93c01b4676831d0ff233a465dd30657cb8ef7e9f
Marc Arnold <m_o_arnold@gmx.de>
2019-01-09 09:20:49 +0100

    Added joinOffice function

--- PfoertnerAdmin/app/src/main/java/de/tu_darmstadt/epool/pfoertneradmin/JoinOfficeActivity.java
+++ PfoertnerAdmin/app/src/main/java/de/tu_darmstadt/epool/pfoertneradmin/JoinOfficeActivity.java
@@ -1,5 +1,6 @@
 package de.tu_darmstadt.epool.pfoertneradmin;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,16 +8,24 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import de.tu_darmstadt.epool.pfoertner.common.ErrorInfoDialog;
 import de.tu_darmstadt.epool.pfoertner.common.QRCodeData;
+import de.tu_darmstadt.epool.pfoertner.common.RequestTask;
+import de.tu_darmstadt.epool.pfoertner.common.retrofit.Authentication;
 import de.tu_darmstadt.epool.pfoertner.common.retrofit.Office;
+import de.tu_darmstadt.epool.pfoertner.common.retrofit.OfficeJoinCode;
+import de.tu_darmstadt.epool.pfoertner.common.retrofit.Person;
+import de.tu_darmstadt.epool.pfoertner.common.retrofit.PersonCreationData;
+import de.tu_darmstadt.epool.pfoertner.common.retrofit.PfoertnerService;
 import de.tu_darmstadt.epool.pfoertneradmin.R;
 import de.tu_darmstadt.epool.pfoertneradmin.tasks.JoinOfficeTask;
+import retrofit2.Retrofit;
 
 public class JoinOfficeActivity extends AppCompatActivity {
 
     private State state = State.getInstance();
     private SharedPreferences settings;
-
+    private PfoertnerService service;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +33,33 @@ public class JoinOfficeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_init2);
 
         settings = getSharedPreferences("Settings", 0);
+        service = state.service;
     }
+
+    private void joinOffice(String lastName, String firstName, Office office) {
+        Context self = this;
+        Authentication authtoken = state.authtoken;
+
+        new RequestTask<Void>() {
+
+
+            @Override
+            protected Void doRequests() {
+                // Join Office
+                //service.joinOffice(authtoken.id ,office.id,new OfficeJoinCode(office.userJoinCode)).execute();
+                Office.joinOffice(authtoken.id, office);
+                // Create Person
+                Person.loadPerson(new PersonCreationData(lastName,firstName), settings, service, authtoken);
+                return null;
+            }
+
+            @Override
+            protected void onException(Exception e) {
+                ErrorInfoDialog.show(self, e.getMessage(), aVoid -> joinOffice());
+            }
+        }.execute();
+    }
+
 
     public void createAccount(View view){
         final EditText firstnameinput = (EditText) findViewById(R.id.VornameInput);
@@ -38,7 +73,11 @@ public class JoinOfficeActivity extends AppCompatActivity {
 
         QRCodeData qrData = QRCodeData.deserialize(getIntent().getStringExtra("QrCodeDataRaw"));
 
+
+        joinOffice(lastName, firstName, new Office(qrData.officeId, qrData.joinCode));
+
         // TODO: Remaining to test, server functionality isnt implemented yet
+
         /*
         new JoinOfficeTask(state.service,
                 settings, state.authtoken,
