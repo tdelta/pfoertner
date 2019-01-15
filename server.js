e68823e7c318360368a3225dca09f019ca8e37a4
Martin <martin_x@live.de>
2019-01-15 09:45:53 +0100

    Found merge error

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ function findOffice(req,res){
     });
 }
 
-server.post(
+server.put(
   '/offices/:officeId/members',
   passport.authenticate('jwt', { session: false }),
   (req,res) => {
@@ -176,57 +176,6 @@ server.get(
           res.status(401).send({message: 'You do not have the permission to access this office.'});
         }
     });
-  }
-);
-
-server.put(
-  '/offices/:officeId/members',
-  (req, res) => {
-    if (req.params.officeId == null) {
-      res.status(400).send({message: 'The given office id is invalid.'});
-    }
-
-    else if (req.body.joinCode == null) {
-      res.status(400).send({message: 'You must provide a valid joinCode.'});
-    }
-
-    else {
-      const officeId = parseInt(req.params.officeId, 10);
-      const joinCode = req.body.joinCode;
-
-      models.Office.findById(officeId).then(
-        office => {
-          if(office) {
-            if(office.joinCode === joinCode){
-                // Create office member that is connected to the logged in device
-                // and the office.
-                createOfficeMember( // TODO: Asynchronität beachten
-                    req.body.firstName,
-                    req.body.lastName,
-                    req.user,
-                    office
-                );
-
-                // Send fcm notification
-                notifyPanel(office.id);
-
-                res.status(200);
-                res.send('Successfully joined office');
-            } else {
-                // Join code is incorrect
-                res.status(401);
-                res.send('Office join code is incorrect');
-            }
-          }
-          
-          else {
-            // Office was not found
-            res.status(404);
-            res.send(`Office with id ${req.params.officeId} does not exist`);
-          }
-        }
-      );
-    }
   }
 );
 
