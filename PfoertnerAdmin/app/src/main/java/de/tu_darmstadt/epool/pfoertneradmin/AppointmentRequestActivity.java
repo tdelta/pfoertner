0f1cfdacfc34ca9827fb7898191def19df811cb5
Martin <martin_x@live.de>
2019-01-30 11:10:24 +0100

    Test implementation for firebase notification

---
+++ PfoertnerAdmin/app/src/main/java/de/tu_darmstadt/epool/pfoertneradmin/AppointmentRequestActivity.java
@@ -0,0 +1,12 @@
+package de.tu_darmstadt.epool.pfoertneradmin;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AppointmentRequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+}
