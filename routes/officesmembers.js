0d3dfc21f2f64a002c6b848c66922b1f28a47c18
Marc Arnold <m_o_arnold@gmx.de>
2019-01-30 09:42:11 +0100

    merge verkackt

--- routes/officesmembers.js
+++ routes/officesmembers.js
@@ -5,6 +5,11 @@ var router = express.Router();
 // Get the required models
 var models = require('../models/models.js');
 
+var notify = require('../notify.js');
+var notifyOfficeSubscribers = notify.notifyOfficeSubscribers;
+
+var auth = require('../authInit.js');
+
 // ONLY FOR DEBUGING/TESTING PURPOSES. REMOVE FOR FINAL SUBMISSION
 // List all users created in the database
 router.get('/', (req, res) => {
@@ -75,6 +80,128 @@ router.get('/:id/picture', (req, res) => {
         // If there is a picture, return 200 and the picture
         res.sendFile('/' + req.params.id + '.jpg', { root: 'uploads' });
       }
+    }
+  });
+});
+
+/**
+ * ENDPOINT: PATCH /officemembers/:id
+ *
+ * Patches general information of an office member
+ */
+router.patch('/:officeMemberId', auth.authFun(), (req, res) => {
+  const officeMemberId = parseInt(req.params.officeMemberId, 10);
+  console.log('Patching officemember');
+
+  authenticateOfficeMember(req, res).then(() => {
+    models.OfficeMember.findById(officeMemberId).then(officemember => {
+      // Only server should set the id
+      delete req.body.id;
+      officemember.update(req.body).then(officemember => {
+        res.status(200);
+        res.send(officemember);
+
+        officemember.getOffice().then(office => {
+          if (office != null) {
+            // TODO optimize to only update the affected member
+            notifyOfficeSubscribers(office, 'OfficeMemberUpdated');
+          } else {
+            console.log(
+              'Could not notify office members, that one of them got updated'
+            );
+          }
+        });
+      });
+    });
+  });
+});
+
+function authenticateOfficeMember(req, res) {
+  return new Promise(function(response) {
+    // Check whether there is a valid officeId in
+    // the request
+    if (req.params.officeMemberId == null) {
+      res.status(400).send({ message: 'The given id is invalid.' });
+    }
+
+    const device = req.user;
+    // The request do not have an correct authorization header
+    if (device === null) {
+      res.status(401).send({
+        message: 'You do not have the permission to access this officemember',
+      });
+    }
+    // The request do have an correct authorization header
+    else {
+      const officeMemberId = parseInt(req.params.officeMemberId, 10);
+
+      device.getOfficeMember().then(loggedIn => {
+        // Check whether a officemember belongs to the authorized device
+        // and whether that officemember is a part of the office
+        if (loggedIn.id === officeMemberId) {
+          console.log('Office member authenticated');
+          response();
+        }
+        // No user belongs to the device or the user belonging to the device
+        // is not the authenticated office member
+        else {
+          res.status(401).send({
+            message:
+              'You do not have the permission to access this officemember.',
+          });
+        }
+      });
+    }
+  });
+}
+
+/**
+ * ENDPOINT: GET /officemembers/:id/picture
+ *
+ * Get the picture of the officemember
+ *
+ */
+router.get('/:id/picture', (req, res) => {
+  const officememberid = parseInt(req.params.id, 10);
+
+  // Get the officemember matching the given id
+  models.OfficeMember.findById(officememberid).then(member => {
+    // If no officemember with this id is found, return 404
+    if (member == null) {
+      res.status('404').send('There is no person to your id');
+    }
+    // There is an officemember matching the id
+    else {
+      // Check wether there is a picture connected to the requested officemember
+      if (member.picture == null) {
+        // If there is no picture, return 404
+        res.status('404').send('There is no picture to your person');
+      } else {
+        // If there is a picture, return 200 and the picture
+        res.sendFile('/' + req.params.id + '.jpg', { root: 'uploads' });
+      }
+    }
+  });
+});
+
+/**
+ * ENDPOINT: PATCH /officemembers/:id/status
+ * Updates the officemember with the status given
+ * in the request body
+ *
+ */
+router.patch('/:id/status', (req, res) => {
+  const officememberid = parseInt(req.params.id, 10);
+  console.log('DEBUG:' + req.body.status);
+  models.OfficeMember.findById(officememberid).then(member => {
+    if (member !== null) {
+      member
+        .update({ status: req.body.status })
+        .then(res.status('200').send('Status has been updated successfully'));
+    } else {
+      res
+        .status('404')
+        .send('There is no officemember matching to the given id');
     }
   });
 });
