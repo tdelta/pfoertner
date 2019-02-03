761e585810966dd0ac382864a36febb48645e0ba
David Heck <heckstrahler@yahoo.de>
2019-02-03 15:13:14 +0100

    weniger hardcoded terminanzeige hoelle

--- PfoertnerPanel/app/src/main/java/de/tu_darmstadt/epool/pfoertnerpanel/ScheduleAppointment.java
+++ PfoertnerPanel/app/src/main/java/de/tu_darmstadt/epool/pfoertnerpanel/ScheduleAppointment.java
@@ -2,6 +2,10 @@ package de.tu_darmstadt.epool.pfoertnerpanel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class ScheduleAppointment extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class ScheduleAppointment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_appointment);
+
+
+        DayView day = (DayView) findViewById(R.id.bla);
+
+        day.setText("bla");
     }
 }
