b3748a5e9c9402e396323130d549a996ea8a85ce
David Heck <heckstrahler@yahoo.de>
2019-02-10 13:56:23 +0100

    ui soweit fertig

--- PfoertnerPanel/app/src/main/java/de/tu_darmstadt/epool/pfoertnerpanel/MakeAppointment.java
+++ PfoertnerPanel/app/src/main/java/de/tu_darmstadt/epool/pfoertnerpanel/MakeAppointment.java
@@ -1,7 +1,10 @@
 package de.tu_darmstadt.epool.pfoertnerpanel;
 
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MakeAppointment extends AppCompatActivity {
@@ -11,8 +14,18 @@ public class MakeAppointment extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_appointment);
         TextView title = findViewById(R.id.textView3);
-        TextView underTitle = findViewById(R.id.textView5);
         title.setText("One last step required");
-        underTitle.setText("Please enter your name, mail and a message. You can also choose your time within the displayed range.");
+        TextView underTitle = findViewById(R.id.textView5);
+        underTitle.setText("Please enter your name, mail and a message.");
+        Button confirm = findViewById(R.id.confirmbutton);
+        confirm.setText("CONFIRM APPOINTMENT");
+        TextInputLayout enterName = findViewById(R.id.textInputLayout);
+        enterName.setHint("Enter your full name here");
+        TextInputLayout enterEmail = findViewById(R.id.textInputLayout6);
+        enterEmail.setHint("Enter your email address here");
+        TextInputLayout enterMessage = findViewById(R.id.textInputLayout7);
+        enterMessage.setHint("Enter a message");
+        TextView appointmentTime = findViewById(R.id.textView6);
+        appointmentTime.setText(getIntent().getStringExtra("appointmentTime"));
     }
 }
