dccfdfe835f26f93988f90898b5cf84cc6a5acb1
Martin <martin_x@live.de>
2019-02-10 03:41:54 +0100

    ...

--- PfoertnerCommon/src/main/java/de/tu_darmstadt/epool/pfoertner/common/CalendarApi.java
+++ PfoertnerCommon/src/main/java/de/tu_darmstadt/epool/pfoertner/common/CalendarApi.java
@@ -1,4 +1,4 @@
-package de.tu_darmstadt.epool.pfoertnerpanel;
+package de.tu_darmstadt.epool.pfoertner.common;
 
 import android.content.Context;
 import android.content.res.AssetManager;
@@ -13,6 +13,8 @@ import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.DateTime;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.calendar.model.CalendarList;
+import com.google.api.services.calendar.model.CalendarListEntry;
 import com.google.api.services.calendar.model.Event;
 import com.google.api.services.calendar.model.Events;
 
@@ -23,9 +25,8 @@ import java.security.PrivateKey;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.Map;
 
-import de.tu_darmstadt.epool.pfoertner.common.PfoertnerApplication;
-import de.tu_darmstadt.epool.pfoertner.common.RequestTask;
 import de.tu_darmstadt.epool.pfoertner.common.synced.observers.MemberObserver;
 import de.tu_darmstadt.epool.pfoertner.common.synced.Member;
 
@@ -33,9 +34,13 @@ public class CalendarApi implements MemberObserver {
 
     private static final String TAG = "Calendar";
     private static final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR_READONLY);
+    private static final String clientId = "626288801350-vk790l2a56u0m25p63q36asu4tv7gnsr.apps.googleusercontent.com";
+    private static final String clientSecret = "wHAYULXTwsZWMQ827ITPIEVr";
+    private static final String credentialsPath = "pfoertner-e43d0751b099.p12";
 
     private final Context context;
     private final NetHttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+    private final PrivateKey key;
 
     private final Member member;
 
@@ -43,14 +48,35 @@ public class CalendarApi implements MemberObserver {
     public CalendarApi(final Member member,final Context context){
         this.context = context;
         this.member = member;
+        key = loadKey();
         member.addObserver(this);
+        Log.d("Access token",member.getAccessToken());
     }
+
+    private RequestTask<String> getCalendarIdTask = new RequestTask<String>(){
+        @Override
+        public String doRequests() throws IOException{
+            return getCalendarId();
+        }
+
+        @Override
+        public void onException(Exception e){
+            Log.d(TAG,"Could not load calendars of the office member");
+            e.printStackTrace();
+        }
+
+        @Override
+        public void onSuccess(String result){
+
+        }
+    };
 
     @Override
     public void onServerAuthCodeChanged(String newServerAuthCode) {
         new RequestTask<String>(){
             @Override
             public String doRequests() throws IOException{
+                Log.d(TAG,"Requesting an auth token from Google");
                 return getAccessToken(newServerAuthCode);
             }
 
@@ -62,10 +88,27 @@ public class CalendarApi implements MemberObserver {
 
             @Override
             public void onSuccess(String result){
+                Log.d(TAG,"Successfully authenticated at Google");
                 PfoertnerApplication app = PfoertnerApplication.get(CalendarApi.this.context);
                 CalendarApi.this.member.setAccessToken(app.getSettings(),result);
+                getCalendarIdTask.execute();
             }
         }.execute();
+    }
+
+    private String getCalendarId() throws IOException{
+        if(member.getAccessToken() == null){
+            throw new RuntimeException("Cannot calendar data before authenticating");
+        }
+        Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JacksonFactory.getDefaultInstance(), getCredential())
+                .setApplicationName("Pfoertner")
+                .build();
+        List<CalendarListEntry> calendarList = service
+                .calendarList().list().execute().getItems();
+        for(CalendarListEntry entry: calendarList){
+            Log.d(TAG,entry.getSummary());
+        }
+        return "lol";
     }
 
     public String getAccessToken(String serverAccessKey) throws IOException{
@@ -74,10 +117,12 @@ public class CalendarApi implements MemberObserver {
                         HTTP_TRANSPORT,
                         JacksonFactory.getDefaultInstance(),
                         "https://oauth2.googleapis.com/token",
-                        context.getResources().getString(R.string.clientId),
-                        context.getResources().getString(R.string.clientSecret),
+                        clientId,
+                        clientSecret,
                         serverAccessKey,
-                        "").execute();
+                        "")
+                        .setScopes(SCOPES)
+                        .execute();
 
         return tokenResponse.getAccessToken();
     }
@@ -86,11 +131,12 @@ public class CalendarApi implements MemberObserver {
         AssetManager assetManager = context.getApplicationContext().getAssets();
 
         try {
-            InputStream stream = assetManager.open(context.getResources().getString(R.string.credentialsPath));
+            // FIXME: There must be a better way to load the private key
+            InputStream stream = assetManager.open(credentialsPath);
             KeyStore p12 = KeyStore.getInstance("pkcs12");
             p12.load(stream, "notasecret".toCharArray());
             Enumeration e = p12.aliases();
-            PrivateKey key = null;
+            PrivateKey key;
             while (e.hasMoreElements()) {
                 String alias = (String) e.nextElement();
                 key = (PrivateKey) p12.getKey(alias, "notasecret".toCharArray());
@@ -106,33 +152,37 @@ public class CalendarApi implements MemberObserver {
         return null;
     }
 
-    public List<Event> getEvents(String calendarId, DateTime start, DateTime end) throws IOException{
+    private Credential getCredential(){
         Credential credential = new GoogleCredential.Builder()
                 .setTransport(HTTP_TRANSPORT)
                 .setJsonFactory(JacksonFactory.getDefaultInstance())
                 .setClientSecrets(
-                        context.getResources().getString(R.string.clientId),
-                        context.getResources().getString(R.string.clientSecret))
+                        clientId,
+                        clientSecret)
                 .setServiceAccountId("110420475534815932936")
                 .setServiceAccountPrivateKeyId("e43d0751b099d3a3186c4477431a1ebf955780f5")
-          //      .setServiceAccountPrivateKey(key)
+                .setServiceAccountPrivateKey(key)
                 .setServiceAccountScopes(SCOPES)
                 .build();
-        //credential.setAccessToken(ACCESS_TOKEN);
+        credential.setAccessToken(member.getAccessToken());
+        return credential;
+    }
 
-        Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JacksonFactory.getDefaultInstance(), credential)
+    public List<Event> getEvents(String calendarId, DateTime start, DateTime end) throws IOException{
+
+        Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JacksonFactory.getDefaultInstance(), getCredential())
                 .setApplicationName("Pfoertner")
                 .build();
 
-        DateTime now = new DateTime(System.currentTimeMillis());
-        Events events = service.events().list("llj7g9m22bmb9201l0rc3bk93o@group.calendar.google.com")
+        Events events = service.events().list(calendarId)
                 .setMaxResults(10)
-                .setTimeMin(now)
+                .setTimeMin(start)
+                .setTimeMax(end)
                 .setOrderBy("startTime")
                 .setSingleEvents(true)
                 .execute();
-        Log.d("Calendar","Executed API access");
-        Log.d("Calendar",events.getItems().toString());
+        Log.d(TAG,"Executed API access");
+        Log.d(TAG,events.getItems().toString());
         return events.getItems();
     }
 }
--- PfoertnerPanel/app/src/main/java/de/tu_darmstadt/epool/pfoertnerpanel/MainActivity.java
+++ PfoertnerPanel/app/src/main/java/de/tu_darmstadt/epool/pfoertnerpanel/MainActivity.java
@@ -1,5 +1,8 @@
 package de.tu_darmstadt.epool.pfoertnerpanel;
 
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.content.Intent;
@@ -17,14 +20,15 @@ import com.google.android.gms.common.GoogleApiAvailability;
 
 import java.util.List;
 
+import de.tu_darmstadt.epool.pfoertner.common.CalendarApi;
 import de.tu_darmstadt.epool.pfoertner.common.ErrorInfoDialog;
 import de.tu_darmstadt.epool.pfoertner.common.PfoertnerApplication;
 import de.tu_darmstadt.epool.pfoertner.common.RequestTask;
 import de.tu_darmstadt.epool.pfoertner.common.SyncService;
 import de.tu_darmstadt.epool.pfoertner.common.retrofit.MemberData;
+import de.tu_darmstadt.epool.pfoertner.common.synced.Member;
 import de.tu_darmstadt.epool.pfoertner.common.synced.observers.MemberObserver;
 import de.tu_darmstadt.epool.pfoertner.common.synced.observers.OfficeObserver;
-import de.tu_darmstadt.epool.pfoertner.common.synced.Office;
 
 public class MainActivity extends AppCompatActivity {
     private final String TAG = "MainActivity";
@@ -86,8 +90,9 @@ public class MainActivity extends AppCompatActivity {
 
         final PfoertnerApplication app = PfoertnerApplication.get(MainActivity.this);
 
-        for (final de.tu_darmstadt.epool.pfoertner.common.synced.Member m : app.getOffice().getMembers()){
+        for (final Member m : app.getOffice().getMembers()){
             this.addMember(m);
+            m.setCalendarApi(new CalendarApi(m,this));
         }
     }
 
@@ -156,12 +161,17 @@ public class MainActivity extends AppCompatActivity {
             public void onStatusChanged(String newStatus) {
                 updateMembers();
             }
+
+            @Override
+            public void onPictureChanged() {
+                updateMembers();
+            }
         };
 
         // TODO: Effizienter, nur für einzelne Members
-        final List<de.tu_darmstadt.epool.pfoertner.common.synced.Member> members = app.getOffice().getMembers();
+        final List<Member> members = app.getOffice().getMembers();
 
-        for (final de.tu_darmstadt.epool.pfoertner.common.synced.Member member : members) {
+        for (final Member member : members) {
             member.deleteObserver(observer);
             member.addObserver(observer);
         }
@@ -237,7 +247,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void addStdMember(View view) {
-        addMember(new MemberData(-1, "Prof. Dr. Ing. Max", "Mustermann", "", ""));
+        addMember(new MemberData(-1, "Prof. Dr. Ing. Max", "Mustermann", "", "Away"), null);
     }
 
     public void removeMembers(){
@@ -249,24 +259,37 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void addMember(final de.tu_darmstadt.epool.pfoertner.common.synced.Member m) {
-        addMember(m.getMemberData());
+    public void addMember(final Member m) {
+        addMember(m.getMemberData(),m);
     }
 
-    public void addMember(final MemberData m){
+    public void addMember(final MemberData memberData, final @Nullable Member member){
         // set the attributes of the member to add
         String[] work = {"Mo-Fr 8:00 - 23:00", "Sa-So 8:00 - 23:00"};
-        Member member = new Member();
+        MemberFragment memberUI = new MemberFragment();
 
-        member.setName(m.firstName + " " + m.lastName);
-        member.setStatus(m.status == null ? "" : m.status);
-        member.setOfficeHours(work);
-        //member.setImage(getDrawable(R.drawable.ic_contact_default));
+        memberUI.setName(memberData.firstName + " " + memberData.lastName);
+        memberUI.setStatus(memberData.status == null ? "" : memberData.status);
+        memberUI.setOfficeHours(work);
+
+        if (member != null) {
+            final PfoertnerApplication app = PfoertnerApplication.get(this);
+
+            memberUI.setPicture(
+                    member
+                        .getPicture(app.getFilesDir())
+                        .map(bitmap -> (Drawable) new BitmapDrawable(this.getResources(), bitmap))
+                        .orElse(
+                                getDrawable(R.drawable.ic_contact_default)
+                        )
+            );
+        }
+        //member.setPicture(getDrawable(R.drawable.ic_contact_default));
 
         switch(memberCount){
             case 0:{
                 FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                transaction.replace(R.id.member_one, member);
+                transaction.replace(R.id.member_one, memberUI);
                 transaction.addToBackStack(null);
                 transaction.commit();
                 memberCount++;
@@ -274,7 +297,7 @@ public class MainActivity extends AppCompatActivity {
             }
             case 1:{
                 FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                transaction.replace(R.id.member_two, member);
+                transaction.replace(R.id.member_two, memberUI);
                 transaction.addToBackStack(null);
 
                 transaction.commit();
@@ -283,7 +306,7 @@ public class MainActivity extends AppCompatActivity {
             }
             case 2:{
                 FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                transaction.replace(R.id.member_three, member);
+                transaction.replace(R.id.member_three, memberUI);
                 transaction.addToBackStack(null);
                 transaction.commit();
                 memberCount++;
@@ -291,7 +314,7 @@ public class MainActivity extends AppCompatActivity {
             }
             case 3:{
                 FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                transaction.replace(R.id.member_four, member);
+                transaction.replace(R.id.member_four, memberUI);
                 transaction.addToBackStack(null);
                 transaction.commit();
                 memberCount++;
