557dffc1c7adabd3ac143c8c15ee97741950af16
Martin <martin_x@live.de>
2019-02-10 02:53:25 +0100

    Added auth code and calendarId fields to officemember

--- models/officemember.js
+++ models/officemember.js
@@ -12,7 +12,9 @@ var OfficeMember = db.sequelize.define('OfficeMember', {
   status: Sequelize.STRING,
   email: Sequelize.STRING,
   picture: Sequelize.STRING,
-  pictureMD5: Sequelize.STRING
+  pictureMD5: Sequelize.STRING,
+  serverAuthCode: Sequelize.STRING,
+  calendarId: Sequelize.STRING,
 });
 
 module.exports = OfficeMember;
