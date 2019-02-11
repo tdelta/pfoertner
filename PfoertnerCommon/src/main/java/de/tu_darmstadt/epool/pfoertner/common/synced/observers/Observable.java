5cb8e85700165e6ebf9cb4b295c9c00fab4684d8
Anton Haubner <anton.haubner@outlook.de>
2019-02-11 13:57:42 +0100

    Fixed crashes on adding or removing observers, while observers are being notified

--- PfoertnerCommon/src/main/java/de/tu_darmstadt/epool/pfoertner/common/synced/observers/Observable.java
+++ PfoertnerCommon/src/main/java/de/tu_darmstadt/epool/pfoertner/common/synced/observers/Observable.java
@@ -6,16 +6,44 @@ import java.util.function.Consumer;
 
 public class Observable<T> {
     private final List<T> observers = new ArrayList<>();
+    private final List<T> newObserversBuffer = new ArrayList<>(0);
+    private final List<T> removedObserversBuffer = new ArrayList<>(0);
+    private boolean observerListLock = false;
 
     public final void addObserver(final T observer) {
-        this.observers.add(observer);
+        if (observerListLock) {
+            this.newObserversBuffer.add(observer);
+        }
+
+        else {
+            this.observers.add(observer);
+        }
     }
 
     public final void deleteObserver(final T observer) {
-        this.observers.remove(observer);
+        if (observerListLock) {
+            this.observers.remove(observer);
+        }
+
+        else {
+            this.removedObserversBuffer.add(observer);
+        }
     }
 
     protected final void notifyEachObserver(final Consumer<T> notificationFunction) {
+        if (observerListLock) {
+            throw new IllegalStateException("You cant execute actions which cause observers to be updated, since observers are already being updated right now.");
+        }
+
+        observerListLock = true;
         this.observers.forEach(notificationFunction);
+
+        this.observers.removeAll(this.removedObserversBuffer);
+        this.removedObserversBuffer.clear();
+
+        this.observers.addAll(this.newObserversBuffer);
+        this.newObserversBuffer.clear();
+
+        observerListLock = false;
     }
 }
