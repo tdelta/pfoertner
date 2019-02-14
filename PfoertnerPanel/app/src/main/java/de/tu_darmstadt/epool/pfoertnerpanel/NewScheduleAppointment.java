54b3e2d9a0fd081edc291e5a9e009f32c1c80bac
David Heck <heckstrahler@yahoo.de>
2019-02-14 09:12:40 +0100

    vergleich fuer zeitslots implementiert

--- PfoertnerPanel/app/src/main/java/de/tu_darmstadt/epool/pfoertnerpanel/NewScheduleAppointment.java
+++ PfoertnerPanel/app/src/main/java/de/tu_darmstadt/epool/pfoertnerpanel/NewScheduleAppointment.java
@@ -29,51 +29,38 @@ public class NewScheduleAppointment extends AppCompatActivity {
     private PfoertnerApplication app;
     private CalendarApi calendarApi;
     private LocalDateTime now;
-    private final int selected = 0xFFFF4081;
     private Timehelpers timehelper;
-
-
     private Member appointmentMember;
     private List<Event> upcommingEvents;
     private LinkedList<DayView> upcommingDayViews;
-
+    private LinearLayout dayviews;
     private LinearLayout timeslots;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_schedule_appointment);
-        LinearLayout dayviews = (LinearLayout) findViewById(R.id.dayviews);
+        dayviews = (LinearLayout) findViewById(R.id.dayviews);
         timehelper = new Timehelpers();
         timeslots = (LinearLayout) findViewById(R.id.timeslots);
         now = LocalDateTime.now();
-
-
-//        app = PfoertnerApplication.get(this);
-//
-//        appointmentMember = app
-//                .getOffice()
-//                .getMembers()
-//                .stream()
-//                .filter(member -> member.getCalendarApi() != null && member.getCalendarId() != null)
-//                .findAny()
-//                .get();
-//
-//        this.calendarApi = appointmentMember.getCalendarApi();
-
-
-
-//        getEventsForTimeslots();
-        //TODO: vorsicht async
-        initializeDayViews(dayviews);
+        app = PfoertnerApplication.get(this);
+        appointmentMember = app
+                .getOffice()
+                .getMembers()
+                .stream()
+                .filter(member -> member.getCalendarApi() != null && member.getCalendarId() != null)
+                .findAny()
+                .get();
+        this.calendarApi = appointmentMember.getCalendarApi();
+        getEventsForTimeslots();
     }
 
     private void recolorDayViews(DayView selectedDayView){
         for (DayView d : upcommingDayViews) {
             d.setColor();
         }
-        selectedDayView.setBackgroundColor(selected);
+        selectedDayView.setBackgroundColor(0xFFFF4081);
     }
 
     private void createTimeSlotsPerDay(DayView dayview){
@@ -85,28 +72,26 @@ public class NewScheduleAppointment extends AppCompatActivity {
         recolorDayViews(dayview);
     }
 
-    private void initializeDayViews(LinearLayout dayviews){
+    private void initializeDayViews(){
         upcommingDayViews = new LinkedList<DayView>();
         for(int i = 0; i < 28; i++){
-
             DayView dayview = new DayView(this);
             dayview.setDate(now.plusDays(i).withHour(0).withMinute(0));
-//            addEventsToDayView(dayview);
+            addEventsToDayView(dayview);
             dayview.setColor();
             dayview.setOnClickListener((View v) -> createTimeSlotsPerDay(dayview));
-
-
-            final FrameLayout.LayoutParams timeSlotMarginParams = (FrameLayout.LayoutParams) dayviews.getLayoutParams();
-            timeSlotMarginParams.setMargins(0, 1, 0, 1);
-            dayviews.addView(dayview, timeSlotMarginParams);
+            final FrameLayout.LayoutParams dayViewMarginParams = (FrameLayout.LayoutParams) dayviews.getLayoutParams();
+            dayViewMarginParams.setMargins(0, 1, 0, 1);
+            dayviews.addView(dayview, dayViewMarginParams);
             upcommingDayViews.add(dayview);
         }
     }
 
     private void addEventsToDayView(DayView dayview){
         for (Event e : upcommingEvents){
-            if(dayview.getDate() == timehelper.toLocalDateTime(e.getStart())){ //TODO: das geht so nicht richtig now <= ex <= ti aber ti at 00:00 ... differenzen testen ob das bei set date auch wirklich so geht
+            if(timehelper.isItToday(dayview.getDate(),timehelper.toLocalDateTime(e.getStart()))){
                 dayview.addEvents(e);
+                upcommingEvents.remove(e);
             }
         }
     }
@@ -117,13 +102,13 @@ public class NewScheduleAppointment extends AppCompatActivity {
             protected List<Event> doRequests() throws Exception {
                 return calendarApi.getEvents(
                         new DateTime(System.currentTimeMillis()),
-                        new DateTime(System.currentTimeMillis() + 86400000 *28)
-                ); //zeitraum 4 wochen
+                        new DateTime(System.currentTimeMillis() + 86400000 *28) //zeitraum 4 wochen
+                );
             }
-
             @Override
             protected void onSuccess(final List<Event> upcommingEvents) {
                 NewScheduleAppointment.this.upcommingEvents = upcommingEvents;
+                initializeDayViews();
             }
 
             @Override
