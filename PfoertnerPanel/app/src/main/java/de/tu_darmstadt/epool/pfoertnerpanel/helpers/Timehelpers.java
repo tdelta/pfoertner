eb48de8227e922c834a3862f16f06db1e238b75f
Marc Arnold <m_o_arnold@gmx.de>
2019-02-19 17:14:00 +0100

    Fix kalanderabfrage für ui

--- PfoertnerPanel/app/src/main/java/de/tu_darmstadt/epool/pfoertnerpanel/helpers/Timehelpers.java
+++ PfoertnerPanel/app/src/main/java/de/tu_darmstadt/epool/pfoertnerpanel/helpers/Timehelpers.java
@@ -1,11 +1,16 @@
 package de.tu_darmstadt.epool.pfoertnerpanel.helpers;
 
+import android.util.Log;
+
 import com.google.api.client.util.DateTime;
 import com.google.api.services.calendar.model.EventDateTime;
 
 import org.threeten.bp.Duration;
 import org.threeten.bp.LocalDateTime;
+import org.threeten.bp.ZoneId;
 import org.threeten.bp.ZoneOffset;
+
+import java.util.TimeZone;
 
 public class Timehelpers {
 
@@ -16,14 +21,14 @@ public class Timehelpers {
         return LocalDateTime.ofEpochSecond(
                 calendarDateTime.getValue() / 1000,
                 0,
-                ZoneOffset.ofHoursMinutes(timeShiftInMinutes / 60, timeShiftInMinutes % 60)
+                ZoneOffset.ofHoursMinutes((timeShiftInMinutes / 60)+1, timeShiftInMinutes % 60)
         );
     }
 
     public static boolean isItToday(final LocalDateTime today, final LocalDateTime timeOfEvent){
         final Duration timeToEvent = Duration.between(today, timeOfEvent);
         final Duration timeToNextDay = Duration.between(today, today.plusDays(1));
-        if(timeToEvent.getSeconds() < timeToNextDay.getSeconds()){
+        if(timeToEvent.getSeconds() >= 0 &&(timeToEvent.getSeconds() < timeToNextDay.getSeconds())){
             return true;
         }else{
             return false;
