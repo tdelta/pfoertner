3cc60dfc940e5b4ba94ad077478ac2a2eccdbb66
Martin <martin_x@live.de>
2019-03-14 16:46:10 +0100

    Forgot to answer googles requests to our webhook, how rude!

--- routes/google-notifications.js
+++ routes/google-notifications.js
@@ -5,13 +5,21 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', (req, res) => {
-  console.log('Received webhook notification from google, Yay');
+  console.log('Received webhook notification from google');
   let channelId = req.header('X-Goog-Channel-ID');
   if (channelId) {
-    models.Device.findOne({ where: { webhookId: channelId } }).then(device => {
-      firebase.sendData(device.id, device.token, { event: 'CalendarUpdated' });
+    models.OfficeMember.findOne({ where: { webhookId: channelId } }).then(officemember => {
+      officemember.getOffice().then(office => {
+        office.getDevice().then(device => {
+          firebase.sendData(device.id, device.fcmToken, {
+            event: 'CalendarUpdated',
+            payload: officemember.calendarId
+          });
+        });
+      });
     });
   }
+  res.status(200).send();
 });
 
 module.exports = router;
