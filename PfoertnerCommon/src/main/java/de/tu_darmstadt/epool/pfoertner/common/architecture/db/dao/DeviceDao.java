9993787b43d8fa5bdd9ce40c1c406701726c1a19
Martin <martin_x@live.de>
2019-03-14 15:22:21 +0100

    Started adding javadoc to PfoertnerCommon

--- PfoertnerCommon/src/main/java/de/tu_darmstadt/epool/pfoertner/common/architecture/db/dao/DeviceDao.java
+++ PfoertnerCommon/src/main/java/de/tu_darmstadt/epool/pfoertner/common/architecture/db/dao/DeviceDao.java
@@ -17,24 +17,47 @@ import io.reactivex.Single;
 
 @Dao
 public abstract class DeviceDao {
+    /**
+     * Inserts a device into the local database, does nothing if a device with the same id already exists
+     * @param device The device to insert
+     * @return The id of the new device, or a value < 0 if an error occured
+     */
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     public abstract long insert(DeviceEntity device);
 
+    /**
+     * Updates an existing device in the database
+     * @param device The device to update
+     */
     @Update
     public abstract void update(DeviceEntity device);
 
+    /**
+     * Updates an existing device or inserts a new one, if it doesnt exist
+     * @param device The device to update or insert
+     */
     @Transaction
     public void upsert(DeviceEntity device) {
         long id = insert(device);
 
         if (id < 0) {
+            // The device already exists
             update(device);
         }
     }
 
+    /**
+     * Loads a LiveData Object (changes in the database can be observed by an activity) for a device
+     * @param deviceId The id of the device to load
+     * @return LiveData containing the device
+     */
     @Query("SELECT * FROM DeviceEntity WHERE id = :deviceId")
     public abstract LiveData<DeviceEntity> load(int deviceId);
 
+    /**
+     * Loads all devices from the local database in a blocking call
+     * @return List of all device in the local database
+     */
     @Query("SELECT * FROM DeviceEntity")
     public abstract List<DeviceEntity> getAllDevices();
 }
--- PfoertnerCommon/src/main/java/de/tu_darmstadt/epool/pfoertner/common/architecture/db/dao/MemberDao.java
+++ PfoertnerCommon/src/main/java/de/tu_darmstadt/epool/pfoertner/common/architecture/db/dao/MemberDao.java
@@ -18,42 +18,95 @@ import static android.arch.persistence.room.OnConflictStrategy.REPLACE;
 
 @Dao
 public abstract class MemberDao {
+    /**
+     * Inserts an office member into the local database, does nothing if an office member with the same id exists
+     * @param member The member to insert
+     * @return The id of the new member, or a value < 0 if an error occured
+     */
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     public abstract long insert(MemberEntity member);
 
+    /**
+     * Updates an existing office member in the local database
+     * @param member The office member to update
+     */
     @Update
     public abstract void update(MemberEntity member);
 
+    /**
+     * Updates an existing office member or inserts a new one, if it doesnt exist
+     * @param member The office member to update or insert
+     */
     @Transaction
     public void upsert(MemberEntity member) {
         long id = insert(member);
 
         if (id < 0) {
+            // Office member already exists
             update(member);
         }
     }
 
+    /**
+     * Inserts a list of office members into the local database, skips office members if one with the same id already exist.
+     * @param members The office members to insert
+     */
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     public abstract void insertMembers(MemberEntity... members);
 
+    /**
+     * Loads a LiveData Object (changes in the database can be observed by an activity) for an office member
+     * @param memberId The id of the office member to load
+     * @return LiveData cointaining the requested office member
+     */
     @Query("SELECT * FROM MemberEntity WHERE id = :memberId")
     public abstract LiveData<MemberEntity> load(int memberId);
 
+    /**
+     * Loads a Flowable Object (changes in the database can be observed by an rxjava observer) for an office member
+     * @param memberId The id of the office member to load
+     * @return Flowable containing the requested office member
+     */
     @Query("SELECT * FROM MemberEntity WHERE id = :memberId")
     public abstract Flowable<MemberEntity> loadFlowable(int memberId);
 
+    /**
+     * Loads a Single Object (data can be asynchronously used after loading is done) for an office member
+     * @param memberId The id of the office member to load
+     * @return Single containing the requested office member
+     */
     @Query("SELECT * FROM MemberEntity WHERE id = :memberId")
     public abstract Single<MemberEntity> loadOnce(int memberId);
 
+    /**
+     * Counts office members in the local database with a specific id
+     * @param memberId Id of the office members
+     * @return The number of office members with the given id
+     */
     @Query("SELECT count(*) FROM MemberEntity WHERE id = :memberId")
     public abstract int countEntities(int memberId);
 
+    /**
+     * Loads all office members from the local database in a blocking call
+     * @return The office members
+     */
     @Query("SELECT * FROM MemberEntity")
     public abstract List<MemberEntity> getAllMembers();
 
+    /**
+     * Loads a LiveData Object (can be observed by an activity) containing all office members in the local database,
+     * that belong to a specified office
+     * @param officeId The id of the office (foreign key in office member)
+     * @return LiveData containing the requested appointments
+     */
     @Query("SELECT * FROM MemberEntity WHERE OfficeId = :officeId")
     public abstract LiveData<List<MemberEntity>> getAllMembersFromOffice(int officeId);
 
+    /**
+     * Load all office members that belong to a given office in a blocking call
+     * @param officeId The id of the office
+     * @return A List of office members
+     */
     @Query("SELECT * FROM MemberEntity WHERE OfficeId = :officeId")
     public abstract List<MemberEntity> getAllMembersFromOfficeOnce(int officeId);
 }
