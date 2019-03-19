2ab57da27aa28f654d40ed97727e4a3766dc9212
Anton Haubner <anton.haubner@outlook.de>
2019-03-19 17:11:23 +0100

    Alten Pfoertner Server entfernt wegen Vorbereitung auf Merge mit Repository des neuen Servers

--- PfoertnerServer/server/models/person.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = function(Person) {
-
-  // Dont allow users to add themselves to an office when creating a person
-  // TODO Person.validatesAbsenceOf('officeId');
-};
